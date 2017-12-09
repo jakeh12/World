c292dd83317d60d8d1c4511bbaadd7bf3993c660
Jakub Hladik <jhladik@iastate.edu>
2017-12-09 13:14:15 -0600

    Initial Commit

---
+++ World/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  World
+//
+//  Created by Jakub Hladik on 12/9/17.
+//  Copyright © 2017 Jakub Hladik. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
