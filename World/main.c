49299b0233ba77fc42c687ffd84c344182ebe50a
Jakub Hladik <jhladik@iastate.edu>
2017-12-20 10:18:14 +0100

    Textures using mipmap.

--- World/main.c
+++ World/main.c
@@ -129,9 +129,12 @@ int main()
     glGenTextures(1, &texture);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     load_texture("textures/crate_diffuse.png");
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glGenerateMipmap(GL_TEXTURE_2D);
     
     /* load shaders */
     GLuint vertex_shader = load_shader(GL_VERTEX_SHADER, "shaders/vshader.glsl");
